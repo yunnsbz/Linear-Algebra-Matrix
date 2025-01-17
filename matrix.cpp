72f1aad00ba80f2f84ade36094cdb8784a1359d3
yunnsbz <yunus21bzkrtc@gmail.com>
2025-01-17 17:12:36 +0300

    updated main function to prepare for a refactor

--- matrix.cpp
+++ matrix.cpp
@@ -1,26 +1,11 @@
 ﻿
-
 #include <iostream>
-#include <time.h>
 #include <stdlib.h>
 #include "matrixClass.h"
 using namespace std;
 
 int main()
 {
-    
-    matrixClass m1(5,5);
-    matrixClass m2(3, 3);
-    m2.randomSet();
-    m1.randomSet();
-    m1.display();
-    m2.display();
-    cout << "determinant m1: " << m1.determinant() << endl;
-    cout << "determinant m2: " << m2.determinant() << endl;
-
-    
-
-
 
 }
 
