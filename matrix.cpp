295eef90a0342f216c34ae46de223f744f8936e2
yunnsbz <yunus21bzkrtc@gmail.com>
2025-03-02 15:57:34 +0300

    merge cleanup

--- matrix.cpp
+++ matrix.cpp
@@ -8,20 +8,6 @@ using namespace std;
 
 int main()
 {
-    
-    MatrixCalculations m1(5,5);
-    MatrixCalculations m2(3, 3);
-    m2.FillRandomValues();
-    m1.FillRandomValues();
-    m1.display();
-    m2.display();
-    cout << "determinant m1: " << m1.Determinant() << endl;
-    cout << "determinant m2: " << m2.Determinant() << endl;
-
-    
-
-
-
 }
 
 
